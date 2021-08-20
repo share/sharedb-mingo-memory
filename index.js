a01a3baabe9295e17b2027ef168a82466339df4f
Christina Burger <christina.burger@lever.co>
2021-08-20 11:01:02 -0400

    Use cloneDeep instead of JSON deep copy to allow regexes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var Mingo = require('mingo');
+var cloneDeep = require('lodash.clonedeep')
 
 // Snapshot properties added to the root doc by `castToDoc()` in sharedb-mongo
 var MONGO_DOC_PROPERTIES = {
@@ -91,7 +92,7 @@ function extendMemoryDB(MemoryDB) {
   };
 
   function parseQuery(inputQuery) {
-    var query = JSON.parse(JSON.stringify(inputQuery));
+    var query = cloneDeep(inputQuery);
 
     if (inputQuery.$orderby)
       console.warn("Warning: query.$orderby deprecated. Use query.$sort instead.");
